5b28676efad42b3bdff0fb3a2658861c08895365
Abei23 <eaa24abar@students.eaaa.dk>
2025-04-23 17:52:40 +0200

    dom content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    // All JavaScript goes inside here
+    const anchor = document.querySelector(".anchor");
+const anchorTooltip = document.getElementById("anchor-tooltip");
+const audio = document.querySelector("audio"); 
+  });
